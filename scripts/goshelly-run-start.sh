40870b14ec6bc5484c1e0678ff3d2157e9dcdb98
Anju Arora <anjumaggie@Anjus-MacBook-Air.local>
2022-07-06 12:34:35 +0530

    mac binary

--- scripts/goshelly-run-start.sh
+++ scripts/goshelly-run-start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-while true; do sleep 1000; done
-# /bin/bash ./bin/app-amd64-linux config
-# /bin/bash ./bin/app-amd64-linux demo
+# while true; do sleep 1000; done
+/bin/bash /goshelly-server/bin/app-amd64-linux config
+/bin/bash /goshelly-server/bin/app-amd64-linux demo
